b71c796cd1d5210dda56140e15b106e8aedc78e4
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2019-07-20 00:51:46 -0400

    add artisan controller & model, create users table

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\ControllerMakeCommand::class,
+        \App\Console\Commands\ModelMakeCommand::class
     ];
 
     /**
