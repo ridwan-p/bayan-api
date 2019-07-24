3246cd39e6dec4943da7d159f27858581b0f3f83
Ridwan Pamungkas <ridwan.pamungks@gmail.com>
2019-07-24 11:11:17 +0700

    add default json response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,8 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected function response($data, $status = true)
+    {
+    	return response()->json(compact('data', 'status'))
+    }
 }
